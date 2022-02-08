3538f6c3efd02cf03e2e2b52c98076b25cf3cf72
shinoj-exe <76904393+shinoj-exe@users.noreply.github.com>
2022-02-08 20:17:13 +0530

    toast notification

---
+++ Toast Notification/script.js
@@ -0,0 +1,29 @@
+const button = document.getElementById("button")
+const toasts = document.getElementById("toasts")
+
+const messages=[
+    'Message one',
+    'Message two',
+    'Message three',
+    'Message four',
+    'Message five',
+]
+
+button.addEventListener("click", () => createNotification())
+
+function createNotification(){
+    const notif = document.createElement("div")
+    notif.classList.add("toast")
+
+    notif.innerText=getRandomMessage()
+
+    toasts.appendChild(notif)
+
+    setTimeout(()=>{
+        notif.remove()
+    },3000)
+}
+
+function getRandomMessage(){
+    return messages[Math.floor(Math.random() * messages.length)]
+}
